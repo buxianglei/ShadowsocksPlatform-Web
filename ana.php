3aabe8bc2ec37e3edf3fd04ad5c52d5d7dcc3795
86533019@qq.com <Aaa19921127>
2017-02-05 17:50:38 +0800

    添加统计代码

--- ana.php
+++ ana.php
@@ -1,3 +1,4 @@
 <div id="analytics-code" style="display:none">
-    统计代码
+    <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+        document.write(unescape("%3Cspan id='cnzz_stat_icon_1261190984'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1261190984%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
 </div>
